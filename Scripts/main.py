fd1116895dc78d835728978db2ce35fd0997fc6f
Juan Antonio Paredes Ahumada <52574469+japareds@users.noreply.github.com>
2023-10-30 16:52:19 +0100

    Included convergence and solutions comparison

--- Scripts/main.py
+++ Scripts/main.py
@@ -9,8 +9,8 @@ Created on Wed Oct 25 13:42:32 2023
 
 import os
 import numpy as np
-import scipy.stats
 import pickle 
+import pandas as pd
 
 import DataSet as DS
 import LowRankBasis as LRB
@@ -63,7 +63,6 @@ class PlacementSolutions():
                 print(f'No solution found for Dopt with {p_zero} reference stations\n')
                 
             
-           
             sensor_placement.C_matrix()
             sensor_placement.covariance_matrix(lowrank_basis.Psi,metric=placement_metric,activate_error_solver=False)
             dict_logdet_Dopt_var[var] = sensor_placement.metric
@@ -90,6 +89,163 @@ class PlacementSolutions():
         
         plots.plot_logdet_vs_variances(dict_logdet_random_var, dict_logdet_Dopt_var, dict_logdet_rankMax_var,
                                        r, n, p_zero, p_empty,save_fig)    
+        
+    def compare_similarities_locations(self,Dopt_path,ds,r,n,p_empty,p_zero,var_eps,alpha_reg,locations_to_compare='empty'):
+        """
+        Load locations obtained with Doptimal for different variances and compare the percentage of similarity
+        with results with Doptimal other variances.
+
+        Parameters
+        ----------
+        Dopt_path : TYPE
+            DESCRIPTION.
+        ds : TYPE
+            DESCRIPTION.
+        r : TYPE
+            DESCRIPTION.
+        n : TYPE
+            DESCRIPTION.
+        p_empty : TYPE
+            DESCRIPTION.
+        p_zero : TYPE
+            DESCRIPTION.
+        alpha_reg : TYPE
+            DESCRIPTION.
+        locations_to_compare : 
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        variances = np.array([1e-6,1e-4,1e-2,1e-1,1e0])
+        lowrank_basis = LRB.LowRankBasis(ds, r)
+        lowrank_basis.snapshots_matrix()
+        lowrank_basis.low_rank_decomposition(normalize=True)
+        locations = {el:[] for el in variances}
+        # Load locations obtained for a given variances ratio
+        for var in variances:
+            sensor_placement = SP.SensorPlacement('D_optimal', n, r, p_zero,n-(p_zero+p_empty),
+                                                  p_empty, var_eps, var)
+            
+            sensor_placement.initialize_problem(lowrank_basis.Psi,alpha_reg)
+            sensor_placement.LoadLocations(Dopt_path, alpha_reg, var)
+            
+            sensor_placement.locations = sensor_placement.dict_locations[p_zero]
+            sensor_placement.weights = sensor_placement.dict_weights[p_zero]
+          
+            if sensor_placement.weights[0].sum() == 0.0 and sensor_placement.weights[1].sum()==0.0:
+                print(f'No solution found for Dopt with {p_zero} reference stations\n')
+                locations[var] = [[],[],[]]
+                
+            else:
+                sensor_placement.C_matrix()
+                sensor_placement.covariance_matrix(lowrank_basis.Psi,metric=placement_metric,activate_error_solver=False)
+                locations[var] = sensor_placement.locations
+            
+        df_comparison = pd.DataFrame(index=[f'{i:.1e}' for i in variances],columns=[f'{i:.1e}' for i in variances])
+        if locations_to_compare == 'empty':
+            idx=2
+            num_locations = p_empty
+        elif locations_to_compare == 'RefSt':
+            idx=1
+            num_locations = p_zero
+        elif locations_to_compare == 'LCSs':
+            idx=0
+            num_locations = n-(p_zero+p_empty)
+          
+       
+        for var in variances:
+            loc1 = locations[var][idx]
+            df_comparison.loc[f'{var:.1e}'][f'{var:.1e}'] = 100. if len(loc1) != 0 else np.nan
+            for var2 in np.delete(variances,np.argwhere(variances==var)):
+                loc2 = locations[var2][idx]
+                df_comparison.loc[f'{var:.1e}'][f'{var2:.1e}'] = 100*np.intersect1d(loc1, loc2).shape[0]/num_locations if (len(loc1) != 0 and len(loc2)!=0) else np.nan
+        
+        print(f'Percentage of shared locations for {num_locations} {locations_to_compare}\n{df_comparison}')
+        return df_comparison
+    
+    def compare_logdet_convergence(self,Dopt_path,ds,r,n,p_empty,p_zero,var_eps,alpha_reg):
+        """
+        Load locations obtained with Doptimal for different variances and compute logdet for smaller variances
+        comparing with the performance of the Doptimal solutions.
+
+        Parameters
+        ----------
+        Dopt_path : TYPE
+            DESCRIPTION.
+        ds : TYPE
+            DESCRIPTION.
+        r : TYPE
+            DESCRIPTION.
+        n : TYPE
+            DESCRIPTION.
+        p_empty : TYPE
+            DESCRIPTION.
+        p_zero : TYPE
+            DESCRIPTION.
+        alpha_reg : TYPE
+            DESCRIPTION.
+        locations_to_compare : 
+
+        Returns
+        -------
+        None.
+
+        """
+        
+        variances = np.array([1e-6,1e-4,1e-2,1e-1,1e0])
+        lowrank_basis = LRB.LowRankBasis(ds, r)
+        lowrank_basis.snapshots_matrix()
+        lowrank_basis.low_rank_decomposition(normalize=True)
+        locations = {el:[] for el in variances}
+        logdet = {el:np.inf for el in variances}
+        # Load locations obtained for a given variances ratio
+        for var in variances:
+            sensor_placement = SP.SensorPlacement('D_optimal', n, r, p_zero,n-(p_zero+p_empty),
+                                                  p_empty, var_eps, var)
+            
+            sensor_placement.initialize_problem(lowrank_basis.Psi,alpha_reg)
+            sensor_placement.LoadLocations(Dopt_path, alpha_reg, var)
+            
+            sensor_placement.locations = sensor_placement.dict_locations[p_zero]
+            sensor_placement.weights = sensor_placement.dict_weights[p_zero]
+          
+            if sensor_placement.weights[0].sum() == 0.0 and sensor_placement.weights[1].sum()==0.0:
+                print(f'No solution found for Dopt with {p_zero} reference stations\n')
+                locations[var] = [[],[],[]]
+                logdet[var] = np.inf
+                
+            else:
+                sensor_placement.C_matrix()
+                sensor_placement.covariance_matrix(lowrank_basis.Psi,metric=placement_metric,activate_error_solver=False)
+                locations[var] = sensor_placement.locations
+                logdet[var] = sensor_placement.metric
+            
+            
+        df_convergence = pd.DataFrame(index=[f'{i:.1e}' for i in variances],columns=[f'{i:.1e}' for i in variances])
+              
+        for var in variances:
+            df_convergence.loc[f'{var:.1e}'][f'{var:.1e}'] = logdet[var]
+            loc = locations[var]
+       
+            for var2 in np.delete(variances,np.argwhere(variances==var)):
+                sensor_placement = SP.SensorPlacement('D_optimal', n, r, p_zero,n-(p_zero+p_empty),
+                                                      p_empty, var_eps, var2)
+                sensor_placement.initialize_problem(lowrank_basis.Psi,alpha_reg)
+                sensor_placement.locations = loc
+                if len(sensor_placement.locations[2]) != 0.0:
+                    sensor_placement.C_matrix()
+                    sensor_placement.covariance_matrix(lowrank_basis.Psi,metric=placement_metric,activate_error_solver=False)
+                    df_convergence.loc[f'{var:.1e}'][f'{var2:.1e}'] = sensor_placement.metric
+                else:
+                    df_convergence.loc[f'{var:.1e}'][f'{var2:.1e}'] = np.inf
+                
+        return df_convergence
+                
+            
+            
         
     def show_histograms(self,r,n,p_empty,p_zero_plot,alpha_reg,solving_algorithm,save_fig):
         plots.plot_locations_evolution(self.Dopt_path,self.rank_path,
@@ -165,7 +321,7 @@ if __name__ == '__main__':
         plots = Plots.Plots(save_path=results_path,marker_size=1,fs_label=3,fs_ticks=7,fs_legend=3,fs_title=10,show_plots=True)
         
         plt.close('all')
-        p_zero_plot = 20
+        p_zero_plot = 40
         
         placement_solutions = PlacementSolutions(Dopt_path, rank_path)
         placement_solutions.compute_solver_failures(r,p_empty,n,1e-6)
@@ -173,6 +329,19 @@ if __name__ == '__main__':
                                                          r,n,p_zero_plot,p_empty,save_fig=True)
         placement_solutions.show_histograms(r, n, p_empty, p_zero_plot, 1e-1, solving_algorithm,save_fig=False)
         
+        # compare different locations
+        df_comparison_empty = placement_solutions.compare_similarities_locations(Dopt_path,dataset.ds_train,
+                                                                                 r,n,p_empty,p_zero_plot,var_eps,alpha_reg,
+                                                                                 locations_to_compare='empty')
+        df_comparison_refst = placement_solutions.compare_similarities_locations(Dopt_path,dataset.ds_train,
+                                                                                 r,n,p_empty,p_zero_plot,var_eps,alpha_reg,
+                                                                                 locations_to_compare='RefSt')
+        df_comparison_lcs= placement_solutions.compare_similarities_locations(Dopt_path,dataset.ds_train,
+                                                                              r,n,p_empty,p_zero_plot,var_eps,alpha_reg,
+                                                                              locations_to_compare='LCSs')        
+        
+        df_logdet_convergence = placement_solutions.compare_logdet_convergence(Dopt_path,dataset.ds_train,
+                                                                               r,n,p_empty,p_zero_plot,var_eps,alpha_reg)
         
   
         
@@ -197,7 +366,7 @@ if __name__ == '__main__':
         plots = Plots.Plots(save_path=results_path,marker_size=1,fs_label=3,fs_legend=4,fs_title=10,show_plots=False)
         
         
-    estimate = True
+    estimate = False
     if estimate:
         Dopt_path = results_path+'Synthetic_Data/TrainingSet_results/Doptimal/'
         rank_path = results_path+'Synthetic_Data/TrainingSet_results/rankMax/'
